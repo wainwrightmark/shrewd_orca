b6a28f44659f64a8f5460545949146acced33f58
Mark Wainwright <wainwrightml@gmail.com>
2022-07-16 12:17:49 +0100

    single expressions no longer do anagrams

--- src/core/question.rs
+++ src/core/question.rs
@@ -11,27 +11,28 @@ impl Question {
     pub fn solve(&self, dict: &WordContext, settings: &SolveSettings) -> Vec<QuestionSolution> {
         match self {
             Question::Expression(ex) => {
-                if ex.words.iter().all(|w| w.as_literal().is_some()) {
-                    let text = ex
-                        .words
-                        .iter()
-                        .map(|wq| wq.as_literal().unwrap().text.clone())
-                        .join("");
+                // if ex.words.iter().all(|w| w.as_literal().is_some()) {
+                //     let text = ex
+                //         .words
+                //         .iter()
+                //         .map(|wq| wq.as_literal().unwrap().text.clone())
+                //         .join("");
 
-                    if text.is_empty() {
-                        return Default::default();
-                    }
-                    dict.anagram_dict
-                        .solve_for_word(text.as_str(), Default::default())
+                //     if text.is_empty() {
+                //         return Default::default();
+                //     }
+                //     dict.anagram_dict
+                //         .solve_for_word(text.as_str(), Default::default())
+                //         .take(settings.max_solutions)
+                //         .map(QuestionSolution::Expression)
+                //         .collect_vec()
+                // } else {
+                    
+                // }
+                ex.solve(dict)
                         .take(settings.max_solutions)
                         .map(QuestionSolution::Expression)
                         .collect_vec()
-                } else {
-                    ex.solve(dict)
-                        .take(settings.max_solutions)
-                        .map(QuestionSolution::Expression)
-                        .collect_vec()
-                }
             }
 
             Question::Equation(eq) => eq
